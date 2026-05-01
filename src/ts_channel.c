dbd4c5b954e7599a0137f737971ad2b52ef47fad
Nate Meyer <nate.devel@gmail.com>
2026-04-30 22:44:53 -0400

    Disable Ref Clock OUT on initiliaztion.

--- src/ts_channel.c
+++ src/ts_channel.c
@@ -204,7 +204,7 @@ int32_t ts_channel_init(tsChannelHdl_t* pTsChannels, file_t ts)
     pChan->pll.clkConf.alternate_select = TS_PLL_INPUT_NONE_SEL;
     pChan->pll.clkConf.in_clks[TS_PLL_REFIN_IDX].enable = 0;
     pChan->pll.clkConf.in_clks[TS_PLL_REFIN_IDX].input_divider = 0;
-    pChan->pll.clkConf.out_clks[TS_PLL_REFOUT_CLK_IDX].enable = 1;
+    pChan->pll.clkConf.out_clks[TS_PLL_REFOUT_CLK_IDX].enable = 0;
     pChan->pll.clkConf.out_clks[TS_PLL_REFOUT_CLK_IDX].output_freq = TS_PLL_REFOUT_RATE_DEFAULT;
     pChan->pll.clkConf.out_clks[TS_PLL_REFOUT_CLK_IDX].output_mode = TS_PLL_REFOUT_CLK_MODE;
     pChan->pll.clkConf.out_clks[TS_PLL_REFOUT_CLK_IDX].output_pll_select = TS_PLL_REFOUT_PLL_MODE;
